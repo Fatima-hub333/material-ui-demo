fc06691c9de06d86a2babff1e8ee89c5f7af8e39
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-03-07 11:46:42 +0500

    add styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CSSBaseline, Grid, Toolbar, Container, CssBaseline } from '@material-ui/core';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CSSBaseline, Grid, Toolbar, Container, CssBaseline } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
       <AppBar position="relative" >
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon}/>
           <Typography variant="h6">
             Photo Album
           </Typography>
         </Toolbar>
       </AppBar>
-
       <main>
-        <div>
+        <div className={classes.container}>
           <Container mxWidth='sm'>
             <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
               Photo Album
@@ -26,16 +25,48 @@ const App = () => {
 
             <Typography variant='h5' align='center' color='textSecondary' paragraph>
               Hello Everyone! This is the photo album I am trying to make it using materia-ui usages. Hello Everyone!
-              This is the photo album I am trying to make it using materia-ui usages. Hello Everyone! This is the photo album I am trying to make it using materia-ui usages.Hello Everyone! This is the photo album I am trying to make it using materia-ui usages.
+              This is the photo album I am trying to make it using materia-ui usages. 
             </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my photos
+                  </Button>
+                </Grid>
+
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    Secondary Action
+                  </Button>
+                </Grid>
+
+              </Grid>
+            </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title = 'Image title'
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant='h5'>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. You can use this section to describe the content.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
 
-        <ButtonGroup variant="contained" aria-label="outlined primary button group">
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
+        </Container>
       </main>
     </>
   )
